9ea02f52214a390916458b9cd087b55008f8ab76
Dheeraj Singh <dhrax21bc@gmail.com>
2022-12-09 08:32:10 +0530

    first commit

--- solution.js
+++ solution.js
@@ -5,7 +5,11 @@ const person = {
       age: 80
     };
 
-// Write your code here, make sure the name of variables is exactly same as mentioned in the problem statement
+const team=players;
+
+const team1=[...players];
+let cap1= {...person};
+
 
 
 export {players,person,team,team1,cap1}
